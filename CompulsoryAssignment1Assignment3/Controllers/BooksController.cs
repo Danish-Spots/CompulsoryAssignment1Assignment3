cf25eea25231a83f54c7738e594d0b27c77013a0
Justin Macdonald Hansen <just1392@edu.easj.dk>
2020-10-14 15:36:53 +0200

    Add project files.

---
+++ CompulsoryAssignment1Assignment3/Controllers/BooksController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CompulsoryAssignment1Assignment1;
+using CompulsoryAssignment1Assignment3.StaticLists;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.SignalR.Protocol;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CompulsoryAssignment1Assignment3.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        // GET: api/<BooksController>
+        [HttpGet]
+        public List<Book> Get()
+        {
+            return BooksList.books;
+        }
+
+        // GET api/<BooksController>/0123456789abc
+        [HttpGet("{isbn13}")]
+        public IActionResult Get(string isbn13)
+        {
+            Book b = getBook(isbn13);
+            if (b == null)
+            {
+                return NotFound(new { message = "No Book Found Corresponding To The Provided ISBN13" });
+            }
+
+            return Ok(b);
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Book value)
+        {
+            Book b = getBook(value.ISBN13);
+            if (b != null)
+            {
+                return Conflict(new {message = "ISBN13 Already In Use"});
+            }
+            BooksList.books.Add(value);
+            return CreatedAtAction("Get", new {isbn13 = value.ISBN13}, value);
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{isbn13}")]
+        public IActionResult Put(string isbn13, [FromBody] Book value)
+        {
+            if (isbn13 != value.ISBN13)
+                return BadRequest(new {message = "Provided ISBN13 Mismatch"});
+
+            Book b = getBook(isbn13);
+
+            if (b != null)
+            {
+                b.Authour = value.Authour;
+                b.PageNumber = value.PageNumber;
+                b.Title = value.Title;
+            }
+            else
+                return NotFound(new {message = "No Book Found Corresponding To The Provided ISBN13"});
+
+            return NoContent();
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{isbn13}")]
+        public IActionResult Delete(string isbn13)
+        {
+            Book b = getBook(isbn13);
+
+            if (b != null)
+                BooksList.books.Remove(b);
+            else
+                return NotFound(new { message = "No Book Found Corresponding To The Provided ISBN13" });
+            return Ok(b);
+        }
+
+        private Book getBook(string isbn13)
+        {
+            Book book = BooksList.books.FirstOrDefault(b => b.ISBN13 == isbn13);
+            return book;
+        }
+    }
+}
---
+++ CompulsoryAssignment1Assignment3/StaticLists/BooksList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CompulsoryAssignment1Assignment1;
+
+namespace CompulsoryAssignment1Assignment3.StaticLists
+{
+    public static class BooksList
+    {
+        public static List<Book> books = new List<Book>()
+        {
+            new Book("To Kill A Mockingbird", "Harper Lee", 296, "9780446310789"),
+            new Book("The Runaway Jury", "John Grisham", 414, "9780440221470"),
+            new Book("My Youth Romantic Comedy Is Wrong, As I Expected Vol 1", "Wataru Watari", 231, "9780316312295")
+        };
+    }
+}
